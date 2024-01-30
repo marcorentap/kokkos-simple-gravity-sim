6a2b439aae6944b8ef6d46b1c4ead808cbcec579
Marc Owen <marcowen20@gmail.com>
2024-01-30 17:59:03 +0900

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,50 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <tuple>
 
+// :)
+#define G 100000
+
+struct object {
+    sf::Vector2f point;
+    float mass;
+};
+
+float CalcGravityForce(float M, float m, float r) {
+    return r ? (G * M * m) / (r * r) : 0;
+}
+
+sf::Vector2f CalcGravityForceVector(struct object &obj1, struct object &obj2) {
+    float x = CalcGravityForce(obj1.mass, obj2.mass, obj1.point.x - obj2.point.x);
+    float y = CalcGravityForce(obj1.mass, obj2.mass, obj1.point.y - obj2.point.y);
+    return sf::Vector2f(x, y);
+}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(500, 500), "some window");
     sf::VertexArray lines(sf::Lines, 1000);
+    struct object thing = {
+        .point = sf::Vector2f(300, 300),
+        .mass = 10
+    };
+    sf::CircleShape circle(thing.point.x, thing.point.y);
+
     for(int i = 0; i < 500; i++) {
-        sf::Vector2f start((i%10)*50, (i/10)*50);
-        sf::Vector2f end(start.x + random()%20, start.y + random()%20);
-        lines.append(start);
+        struct object start = {
+            .point = sf::Vector2f((i%10)*50, (i/10)*50),
+            .mass = 1
+        };
+        if (start.point.x > 500 || start.point.y > 500) break;
+
+        sf::Vector2f force = CalcGravityForceVector(thing, start);
+        sf::Vector2f end(start.point.x + force.x, start.point.y + force.y);
+        printf("point (%f, %f) has force (%f, %f)\n", start.point.x, start.point.y, force.x, force.y);
+
+        lines.append(start.point);
         lines.append(end);
     }
  
@@ -25,6 +58,7 @@ int main() {
         }
         window.clear();
     window.draw(lines);
+    window.draw(circle);
         window.display();
     }
     return 0;
