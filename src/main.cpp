1a57cce3221fde4c003a760ba92a1c51feb6cbe2
Marc Owen <marcowen20@gmail.com>
2024-01-30 22:52:27 +0900

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,35 @@
 // #include <iostream>
 // #include <tuple>
 
+class MassedObject {
+  public:
+    sf::CircleShape shape;
+    float mass;
+    sf::Vector2f pos;
+    MassedObject(float radius, float mass, sf::Vector2f pos)
+        : shape(radius), mass(mass), pos(pos) {
+        SetPosition(pos);
+    }
+
+    void SetPosition(sf::Vector2f pos) {
+        shape.setPosition(pos);
+        shape.setOrigin(shape.getRadius(), shape.getRadius());
+    }
+};
+
 class GravityGrid : public ss::PointGrid {
   public:
     GravityGrid(int xSize, int ySize, int xCount, int yCount)
         : ss::PointGrid(xSize, ySize, xCount, yCount) {}
 
-    void Update() {
-        return;
-    }
+    void Update() { return; }
 };
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(720, 720), "simplesim");
+    
     GravityGrid gravGrid(window.getSize().x, window.getSize().y, 50, 50);
+    MassedObject obj(10, 30, sf::Vector2f(300, 300));
 
     while (window.isOpen()) {
         sf::Event event;
@@ -28,10 +44,19 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+
+        }
+
+        auto mousePos = sf::Mouse::getPosition();
+        printf("mouse at (%f, %f)\n", mousePos.x, mousePos.y);
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            auto mousePos = sf::Mouse::getPosition();
+            obj.SetPosition((sf::Vector2f) mousePos);
         }
 
         window.clear();
         gravGrid.Draw(window);
+        window.draw(obj.shape);
         window.display();
     }
 
