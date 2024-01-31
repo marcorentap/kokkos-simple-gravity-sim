57b53ecab0bec47bee4f25a52bd2d4818ddd5344
Marc Owen <marcowen20@gmail.com>
2024-01-31 12:38:20 +0900

    Now working with multiple bodies

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Window/Keyboard.hpp"
 #include "simplesim/grid.hpp"
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
@@ -13,96 +14,129 @@
 // #include <tuple>
 
 class MassedObject {
-public:
-  sf::CircleShape shape;
-  float mass;
-  sf::Vector2f pos;
-  MassedObject(float radius, float mass, sf::Vector2f pos)
-      : shape(radius), mass(mass), pos(pos) {
-    SetPosition(pos);
-  }
+  public:
+    sf::CircleShape shape;
+    float mass;
+    sf::Vector2f velocity;
+    sf::Vector2f acceleration;
+    sf::Vector2f pos;
+    MassedObject(float radius, float mass, sf::Vector2f pos)
+        : shape(radius), mass(mass), pos(pos) {
+        SetPosition(pos);
+    }
 
-  void SetPosition(sf::Vector2f pos) {
-    this->pos = pos;
-    shape.setPosition(pos);
-    shape.setOrigin(shape.getRadius(), shape.getRadius());
-  }
+    void SetPosition(sf::Vector2f pos) {
+        this->pos = pos;
+        shape.setPosition(pos);
+        shape.setOrigin(shape.getRadius(), shape.getRadius());
+    }
 };
 
 class GravityGrid : public ss::PointGrid {
-private:
-  int pointMass = 1;
-  int G = 1000000;
-  int forceScale = 10;
-  float lineMax = 50;
+  private:
+    int pointMass = 1;
+    int G = 1000000;
+    int forceScale = 10;
+    float lineMax = 10;
 
-  float CalculateGravity(MassedObject M, MassedObject m) {
-    float diffX = M.pos.x - m.pos.x;
-    float diffY = M.pos.y - m.pos.y;
-    float r = std::sqrt(std::pow(diffX, 2) + std::pow(diffY, 2));
-    return r ? (G * M.mass * m.mass) / pow(r, 2) : 0;
-  }
-  float CalculateGravity(MassedObject M, ss::GridPoint point) {
-    return CalculateGravity(
-        M, MassedObject(pointMass, pointMass, point.point.getPosition()));
-  }
-
-public:
-  GravityGrid(int xSize, int ySize, int xCount, int yCount)
-      : ss::PointGrid(xSize, ySize, xCount, yCount) {}
-
-  void Update(std::vector<MassedObject> massedObjects) {
-    for (auto &point : this->points) {
-      auto obj = massedObjects[0];
-      auto force = CalculateGravity(obj, point);
-      auto &startPos = point.line[0].position;
-      auto &endPos = point.line[1].position;
-
-      float diffX = obj.pos.x - startPos.x;
-      float diffY = obj.pos.y - startPos.y;
-      float dist = std::sqrt(std::pow(diffX, 2) + std::pow(diffY, 2));
-      sf::Vector2f unit(diffX / dist, diffY / dist);
-      endPos.x = startPos.x + unit.x * std::min(force, lineMax);
-      endPos.y = startPos.y + unit.y * std::min(force, lineMax);
-
-      std::cout << "point (" << startPos.x << ", " << startPos.y << ")-";
-      std::cout << "point (" << endPos.x << ", " << endPos.y << ")";
-      std::cout << " force " << force << std::endl;
+    float CalculateGravity(MassedObject M, MassedObject m) {
+        float diffX = M.pos.x - m.pos.x;
+        float diffY = M.pos.y - m.pos.y;
+        float r = std::sqrt(std::pow(diffX, 2) + std::pow(diffY, 2));
+        return r ? (G * M.mass * m.mass) / pow(r, 2) : 0;
     }
-    return;
-  }
-
-  void PrintDebug() {
-    for (auto &point : this->points) {
+    float CalculateGravity(MassedObject M, ss::GridPoint point) {
+        return CalculateGravity(
+            M, MassedObject(pointMass, pointMass, point.point.getPosition()));
     }
-  }
+
+  public:
+    GravityGrid(int xSize, int ySize, int xCount, int yCount)
+        : ss::PointGrid(xSize, ySize, xCount, yCount) {}
+
+    void Update(std::vector<MassedObject> massedObjects) {
+        for (auto &point : this->points) {
+            auto &startPos = point.line[0].position;
+            auto &endPos = point.line[1].position;
+            endPos = startPos;
+            // printf("point start(%f, %f) ", startPos.x, startPos.y);
+            for (auto &obj : massedObjects) {
+                auto force = CalculateGravity(obj, point);
+                float diffX = obj.pos.x - startPos.x;
+                float diffY = obj.pos.y - startPos.y;
+                float dist = std::sqrt(std::pow(diffX, 2) + std::pow(diffY, 2));
+                sf::Vector2f unit(diffX / dist, diffY / dist);
+                endPos += unit * std::min(std::abs(force), lineMax);
+            }
+        }
+        return;
+    }
+
+    void PrintDebug() {
+        for (auto &point : this->points) {
+        }
+    }
 };
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(1000, 1000), "simplesim");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "simplesim");
 
-  GravityGrid gravGrid(window.getSize().x, window.getSize().y, 30, 30);
-  MassedObject obj(10, 100, sf::Vector2f(300, 300));
+    GravityGrid gravGrid(window.getSize().x, window.getSize().y, 80, 80);
+    std::vector<MassedObject> objects = {
+        MassedObject(10, 100, sf::Vector2f(300, 300)),
+        MassedObject(10, 100, sf::Vector2f(700, 300)),
+        MassedObject(10, 100, sf::Vector2f(500, 500)),
+    };
+    auto selectedObj = &objects[0];
 
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::MouseButtonPressed) {
+                // Left click selects an object
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    auto mousePos = sf::Mouse::getPosition(window);
+                    for (auto &obj : objects) {
+                        if (obj.pos.x < mousePos.x + obj.shape.getRadius() &&
+                            obj.pos.y < mousePos.y + obj.shape.getRadius() &&
+                            obj.pos.x > mousePos.x - obj.shape.getRadius() &&
+                            obj.pos.y > mousePos.y - obj.shape.getRadius()) {
+                            selectedObj = &obj;
+                            printf("selected obj(%f, %f)\n", obj.pos.x,
+                                   obj.pos.y);
+                        }
+                    }
+                }
+
+                // Right click deselect object
+                if (event.mouseButton.button == sf::Mouse::Right) {
+                    selectedObj = NULL;
+                    printf("No object selected\n");
+                }
+            }
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            auto mousePos = sf::Mouse::getPosition(window);
+            if (selectedObj) {
+                selectedObj->SetPosition((sf::Vector2f)mousePos);
+                printf("move obj(%f, %f) to (%d,%d)\n", selectedObj->pos.x,
+                       selectedObj->pos.y, mousePos.x, mousePos.y);
+            }
+        }
+
+        window.clear();
+        gravGrid.Update(objects);
+        gravGrid.Draw(window);
+        for (auto obj : objects) {
+            window.draw(obj.shape);
+        }
+        window.display();
     }
 
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-      auto mousePos = sf::Mouse::getPosition(window);
-      obj.SetPosition((sf::Vector2f)mousePos);
-    }
-
-    window.clear();
-    gravGrid.Update({obj});
-    gravGrid.Draw(window);
-    window.draw(obj.shape);
-    window.display();
-  }
-
-  return 0;
+    return 0;
 }
